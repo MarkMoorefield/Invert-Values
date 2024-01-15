976e4d8e91c5943f81ac41054509f5ed682c5a93
MarkMoorefield <155267237+MarkMoorefield@users.noreply.github.com>
2024-01-15 23:57:35 +0000

    Added comments explaining the solution.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// I created a new array where I can store the negative integers.
+// I then created a for loop that runs through every element in the array.
+// Once it gets to an element it multiplies it by -1 and pushes it to the new array. 
+// Finally I returned the array.
 function invert(array) {
     let newArr = [];
      for (let i = 0; i < array.length; i++) {
